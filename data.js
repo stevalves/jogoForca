9ed77e4a9e15f523f72984969c85a798814a3822
Estevao Alves <ealves1710@hotmail.com>
2022-09-14 00:36:29 -0300

    Treino DOM: Jogo da forca

---
+++ data.js
@@ -0,0 +1,134 @@
+let data = [
+    {
+        nome: "BANANA",
+        tipo: "Fruta",
+        dificuldade: "Fácil",
+        id: 0
+    },
+    {
+        nome: "PERA",
+        tipo: "Fruta",
+        dificuldade: "Fácil",
+        id: 1
+    },
+    {
+        nome: "UVA",
+        tipo: "Fruta",
+        dificuldade: "Fácil",
+        id: 2
+    },
+    {
+        nome: "LIMAO",
+        tipo: "Fruta",
+        dificuldade: "Fácil",
+        id: 3
+    },
+    {
+        nome: "KIWI",
+        tipo: "Fruta",
+        dificuldade: "Médio",
+        id: 4
+    },
+    {
+        nome: "ABACAXI",
+        tipo: "Fruta",
+        dificuldade: "Fácil",
+        id: 5
+    },
+    {
+        nome: "TANGERINA",
+        tipo: "Fruta",
+        dificuldade: "Médio",
+        id: 6
+    },
+    {
+        nome: "ABACATE",
+        tipo: "Fruta",
+        dificuldade: "Médio",
+        id: 7
+    },
+    {
+        nome: "PESSEGO",
+        tipo: "Fruta",
+        dificuldade: "Médio",
+        id: 8
+    },
+    {
+        nome: "PITAIA",
+        tipo: "Fruta",
+        dificuldade: "Difícil",
+        id: 9
+    },
+    {
+        nome: "PITAIA",
+        tipo: "Fruta",
+        dificuldade: "Difícil",
+        id: 10
+    },
+    {
+        nome: "FRAMBOESA",
+        tipo: "Fruta",
+        dificuldade: "Difícil",
+        id: 11
+    },
+    {
+        nome: "GRAVIOLA",
+        tipo: "Fruta",
+        dificuldade: "Difícil",
+        id: 12
+    },
+    {
+        nome: "GROSELHA",
+        tipo: "Fruta",
+        dificuldade: "Difícil",
+        id: 13
+    },
+    {
+        nome: "GOIABA",
+        tipo: "Fruta",
+        dificuldade: "Difícil",
+        id: 14
+    },
+    {
+        nome: "AMORA",
+        tipo: "Fruta",
+        dificuldade: "Médio",
+        id: 15
+    },
+    {
+        nome: "AZUL",
+        tipo: "Cor",
+        dificuldade: "Fácil",
+        id: 16
+    },
+    {
+        nome: "VERMELHO",
+        tipo: "Cor",
+        dificuldade: "Fácil",
+        id: 17
+    },
+    {
+        nome: "AMARELO",
+        tipo: "Cor",
+        dificuldade: "Fácil",
+        id: 18
+    },
+    {
+        nome: "VERDE",
+        tipo: "Cor",
+        dificuldade: "Fácil",
+        id: 19
+    },
+    {
+        nome: "CIANO",
+        tipo: "Cor",
+        dificuldade: "Médio",
+        id: 20
+    },
+    {
+        nome: "PURPURO",
+        tipo: "Cor",
+        dificuldade: "Difícil",
+        id: 20
+    },
+]
---
+++ script.js
@@ -0,0 +1,106 @@
+let teclado = document.querySelector('.teclado')
+let repeat = document.querySelector('.repeat')
+
+function conferirLetra(letra) {
+    let index = []
+    for (let i = 0; i < repeat.innerText.length; i++) {
+        index.push(repeat.innerText[i])
+        if (index.includes(letra)) {
+            alert('letra repetida')
+            if (desenho.children[0].className == 'head') {
+                desenho.children[0].className = 'headNull'
+            } else if (desenho.children[0].className == 'head1') {
+                desenho.children[0].className = 'head'
+            } else if (desenho.children[0].className == 'head2') {
+                desenho.children[0].className = 'head1'
+            } else if (desenho.children[0].className == 'head3') {
+                desenho.children[0].className = 'head2'
+            } else if (desenho.children[0].className == 'head4') {
+                desenho.children[0].className = 'head3'
+            } else if (desenho.children[0].className == 'head5') {
+                desenho.children[0].className = 'head4'
+            } else if (desenho.children[0].className == 'headDie') {
+                desenho.children[0].className = 'head5'
+            }
+            return
+        }
+    }
+    repeat.innerText += letra
+    return
+}
+
+let letras = []
+let letra = ""
+let resposta = document.querySelector('.resposta')
+let tipo = document.querySelector('.tipo')
+let dificuldade = document.querySelector('.dificuldade')
+function selectData(data) {
+    let escolha = Number(Math.random() * (data.length - 0) - 0).toFixed(0)
+    for (let j = 0; j < data.length; j++) {
+        if (data[j].id == escolha) {
+            for (let p = 0; p < data[j].nome.length; p++) {
+                let p = document.createElement('p')
+                p.innerText = "_"
+                resposta.appendChild(p)
+                letra = data[j].nome
+                tipo.innerText = data[j].tipo
+                dificuldade.innerText = data[j].dificuldade
+            }
+        }
+    }
+}
+selectData(data)
+console.log(letra)
+let desenho = document.querySelector('.desenho')
+
+for (let o = 0; o < letra.length; o++) {
+    letras.push(letra[o])
+}
+
+teclado.addEventListener('click', function (e) {
+    if (e.path[0].className == 'tecla') {
+        conferirLetra(e.path[0].id)
+        for (let o = 0; o < letra.length; o++) {
+            if (letra[o] == e.path[0].id) {
+                resposta.children[o].innerText = e.path[0].id
+            }
+        }
+    }
+    if (e.path[0].className == 'tecla') {
+        if (!letras.includes(e.path[0].id)) {
+            if (desenho.children[0].className == 'headNull') {
+                desenho.children[0].className = 'head'
+            } else if (desenho.children[0].className == 'head') {
+                desenho.children[0].className = 'head1'
+            } else if (desenho.children[0].className == 'head1') {
+                desenho.children[0].className = 'head2'
+            } else if (desenho.children[0].className == 'head2') {
+                desenho.children[0].className = 'head3'
+            } else if (desenho.children[0].className == 'head3') {
+                desenho.children[0].className = 'head4'
+            } else if (desenho.children[0].className == 'head4') {
+                desenho.children[0].className = 'head5'
+            } else if (desenho.children[0].className == 'head5') {
+                desenho.children[0].className = 'headDie'
+                alert(`PERDEU! Resposta certa = ${letra}`)
+                window.location.reload(true)
+            }
+        }
+    }
+})
+
+let ta = document.querySelector('.ta')
+let tey = document.querySelector('.tey')
+
+tey.addEventListener('click', function (e) {
+    if (ta.value == letra) {
+        alert(`CERTA RESPOSTA!!!`)
+        window.location.reload(true)
+    }else if (ta.value == letra.toLowerCase()) {
+        alert(`CERTA RESPOSTA!!!`)
+        window.location.reload(true)
+    }else {
+        alert(`PERDEU! Resposta certa = ${letra}`)
+        window.location.reload(true)
+    }
+})
